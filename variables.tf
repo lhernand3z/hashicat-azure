0cc8b125159983566fcb6e66ca33a331713c6a92
Luis Hernandez <lhernand3z@gmail.com>
2022-08-19 03:03:29 +0000

    Added network module again 2

--- variables.tf
+++ variables.tf
@@ -74,4 +74,3 @@ variable "placeholder" {
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
 
-variable "resource_group_name" {}
